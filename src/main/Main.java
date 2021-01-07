220294d2c6f4dfac75a4b4d371e9f34ce9d0df1d
0321mportoles <62716251+0321mportoles@users.noreply.github.com>
2021-01-07 13:11:44 +0100

    Añadido main principal

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) {
+		
+    }
+
+}
+
